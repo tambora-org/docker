c5000192a69f30be8198718e794b33256b3c8368
Unknown <michael.kahle@yahoo.de>
2018-12-03 14:50:30 +0100

    Create subdirs if needed

--- crePhp/cre/php-entrypoint.sh
+++ crePhp/cre/php-entrypoint.sh
@@ -2,6 +2,16 @@
 set -e
 
 # TODO: If CRE_PHP_ROOT not set: use www
+if [[ -z $CRE_PHP_ROOT ]]; then
+  CRE_PHP_ROOT='www';
+fi
+
+
+if [[! -d /cre/$CRE_PHP_ROOT ]]
+then
+  mkdir -p "/cre/$CRE_PHP_ROOT"
+fi
+
 cp -f /cre/db-test.php "/cre/$CRE_PHP_ROOT/db-test.php"
 
 exec "$@"
