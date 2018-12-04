82d0088aaff315dccd9e95e9741fff5ecc8e4904
Unknown <michael.kahle@yahoo.de>
2018-12-04 10:46:26 +0100

    php survey misses appliction directory

--- crePhpSurvey/cre/survey-entrypoint.sh
+++ crePhpSurvey/cre/survey-entrypoint.sh
@@ -2,10 +2,12 @@
 set -e 
 
 if [[ -n $MYSQL_DB ]]; then
+  mkdir -p /cre/www/survey/application/config 
   cp -f /cre/config-mysql.php /cre/www/survey/application/config/config.php
 fi
 if [[ -n $POSTGRES_DB ]]; then
-  cp -f /cre/config-postgres.php /cre/www/survey/application/config/config.php
+  mkdir -p /cre/www/survey/application/config
+  cp -f /cre/config-pgsql.php /cre/www/survey/application/config/config.php
 fi
 
 exec "$@"
