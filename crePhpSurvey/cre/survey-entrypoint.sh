29c1e8af78b84f11b7ce42fb574ff8efb7852767
Unknown <michael.kahle@yahoo.de>
2018-12-06 12:16:34 +0100

    Use rsync also for survey

--- crePhpSurvey/cre/survey-entrypoint.sh
+++ crePhpSurvey/cre/survey-entrypoint.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -e 
 
+rsync -rl /cre/tmp/survey/ /cre/www/survey
+# later use --exclude /dir1/ --exclude /dir2/
+rm -rf /cre/tmp/survey
+
 if [[ -n $MYSQL_DB ]]; then
   mkdir -p /cre/www/survey/application/config 
   cp -f /cre/config-mysql.php /cre/www/survey/application/config/config.php
