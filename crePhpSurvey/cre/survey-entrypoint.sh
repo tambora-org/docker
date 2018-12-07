4ed999b10e560ffbed18a0f313574980112dd8d3
Unknown <michael.kahle@yahoo.de>
2018-12-07 11:01:20 +0100

    chmod directories

--- crePhpSurvey/cre/survey-entrypoint.sh
+++ crePhpSurvey/cre/survey-entrypoint.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 set -e 
 
-rsync -rl /cre/tmp/survey/ /cre/www/survey
+rsync -rl /cre/tmp/survey/limesurvey/ /cre/www/survey
 # later use --exclude /dir1/ --exclude /dir2/
 rm -rf /cre/tmp/survey
+chown -R www-data:www-data /cre/www/survey
+chmod -R 774 /cre/www/survey/tmp
+chmod -R 774 /cre/www/survey/application/config
+chmod -R 774 /cre/www/survey/upload 
+
 
 if [[ -n $MYSQL_DB ]]; then
   mkdir -p /cre/www/survey/application/config 
