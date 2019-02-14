7f925216d0736433299a436b40e77079ddbd46f5
Unknown <michael.kahle@yahoo.de>
2019-02-14 10:04:05 +0100

    Add path to binaries

--- crePostgres/cre/postgres-entrypoint.sh
+++ crePostgres/cre/postgres-entrypoint.sh
@@ -5,22 +5,10 @@ set -e
 #chown -R postgres:postgres /tmp/docker.sock
 
 POSTGRES_ROOT_PWD=${POSTGRES_ROOT_PWD:-"postgres"}
-#POSTGRES_ROOT_PWD=${POSTGRES_ROOT_PWD:-$(pwgen -c -n -1 14)}
-export RANDOM_TEXT=$(pwgen -c -n -1 14)}  
-#problem: env not available outside
-
-
-echo "1* RANDOM_DOCKER_DEFAULT: $RANDOM_DOCKER_DEFAULT"
-echo "1* RANDOM_DOCKER_RUN: $RANDOM_DOCKER_RUN"
-echo "1* RANDOM_TEXT: $RANDOM_TEXT"
-
-echo "2* RANDOM_DOCKER_DEFAULT: $RANDOM_DOCKER_DEFAULT"
-echo "2* RANDOM_DOCKER_RUN: $RANDOM_DOCKER_RUN"
-echo "2* RANDOM_TEXT: $RANDOM_TEXT"
 
 echo "Setting up new power user credentials."
 
-POSTGRESQL_BIN=/usr/lib/postgresql/${POSTGRES_VERSION}/bin/postgres
+POSTGRESQL_BIN=/usr/lib/postgresql/${POSTGRES_VERSION}/bin
 POSTGRESQL_CONFIG_FILE=/etc/postgresql/${POSTGRES_VERSION}/main/postgresql.conf
 POSTGRESQL_DATA=/var/lib/postgresql/${POSTGRES_VERSION}/main
 
@@ -29,7 +17,7 @@ if [ ! -d "$POSTGRESQL_DATA" ]; then
 
 echo "Run initdb..."
 
-  sudo -u postgres -H /usr/lib/postgresql/${POSTGRES_VERSION}/bin/initdb \
+  sudo -u postgres -H ${POSTGRESQL_BIN}/initdb \
     --pgdata=/var/lib/postgresql/${POSTGRES_VERSION}/main --pwfile=/var/lib/postgresql/pwfile \
     --username=postgres --encoding=unicode --auth=trust >/dev/null
 
--- crePostgres/cre/run.sh
+++ crePostgres/cre/run.sh
@@ -2,11 +2,11 @@
 
 ##POSTGRES_VERSION=9.5 
 
-POSTGRESQL_BIN=/usr/lib/postgresql/${POSTGRES_VERSION}/bin/postgres
+POSTGRESQL_BIN=/usr/lib/postgresql/${POSTGRES_VERSION}/bin
 POSTGRESQL_CONFIG_FILE=/etc/postgresql/${POSTGRES_VERSION}/main/postgresql.conf
 POSTGRESQL_DATA=/var/lib/postgresql/${POSTGRES_VERSION}/main
 
 echo "start postgres server"
 
-exec sudo -u postgres $POSTGRESQL_BIN -D $POSTGRESQL_DATA -c config_file=$POSTGRESQL_CONFIG_FILE
-#exec $POSTGRESQL_BIN -D $POSTGRESQL_DATA -c config_file=$POSTGRESQL_CONFIG_FILE
+exec sudo -u postgres ${POSTGRESQL_BIN}/postgres -D $POSTGRESQL_DATA -c config_file=$POSTGRESQL_CONFIG_FILE
+#exec ${POSTGRESQL_BIN}/postgres -D $POSTGRESQL_DATA -c config_file=$POSTGRESQL_CONFIG_FILE
