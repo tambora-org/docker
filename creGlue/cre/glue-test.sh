97923bf084d733505374d4a1694813012d298c0a
Unknown <michael.kahle@yahoo.de>
2019-02-14 15:20:05 +0100

    Allow execution of test

--- creGlue/cre/glue-test.sh
+++ creGlue/cre/glue-test.sh
@@ -1,4 +1,5 @@
 sleep 5
+
 if [ ! -f /cre/versions.txt ]; then
     echo "[FAIL]: File /cre/versions.txt not found!"
     exit 1
@@ -11,6 +12,16 @@ fi
 actualsize=$(wc -c <"/cre/glue/test.txt")
 if [ ! $actualsize -ge 200 ]; then
     echo "[FAIL]: File /cre/glue/test.txt is too small!"
+    exit 1
+fi
+
+if [! grep "docker-gen" /cre/versions.txt > /dev/null]; then
+    echo "[FAIL]: docker-gen not installed!"
+    exit 1
+fi
+
+if [! grep -P "docker-gen \T DOCKER_GEN_VERSION" /cre/versions.txt > /dev/null]; then
+    echo "[FAIL]: Wrong version of docker-gen installed!"
     exit 1
 fi
 
