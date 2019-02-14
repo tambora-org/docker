0d6af81b5734536b68502e7c2b5820e38a8cf1ff
Unknown <michael.kahle@yahoo.de>
2019-02-14 17:20:00 +0100

    versioning and testing

---
+++ crePostgres/cre/postgres-test.sh
@@ -0,0 +1,35 @@
+sleep 2
+
+if [ ! -f /cre/versions.txt ]; then
+    echo "[FAIL]: File /cre/versions.txt not found!"
+    exit 1
+fi
+
+if [ ! -f /cre/postgres-procfile ]; then
+    echo "[FAIL]: File /cre/postgres-procfile not found!"
+    exit 1
+fi
+
+if [! grep "postgres (PostgreSQL)" /cre/versions.txt > /dev/null]; then
+    echo "[FAIL]: postgres not installed!"
+    exit 1
+fi
+
+if [! grep -P "postgres (PostgreSQL) POSTGRES_VERSION" /cre/versions.txt > /dev/null]; then
+    echo "[WARNING]: Wrong version of postgres installed!"
+    #exit 1
+fi
+
+if [! grep "psql (PostgreSQL)" /cre/versions.txt > /dev/null]; then
+    echo "[FAIL]: psql not installed!"
+    exit 1
+fi
+
+#shoreman /cre/postgres-procfile &
+
+sleep 2
+
+#Do some fancy tests....?
+
+echo "[SUCCESS]"
+exit 0
