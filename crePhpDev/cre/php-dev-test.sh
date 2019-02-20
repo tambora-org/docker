28c3add734b9d1631ba197d800496e3c61517b9f
Unknown <michael.kahle@yahoo.de>
2019-02-20 11:41:35 +0100

    Update to new standard

---
+++ crePhpDev/cre/php-dev-test.sh
@@ -0,0 +1,30 @@
+sleep 2
+
+if [ ! -f /cre/versions.txt ]; then
+    echo "[FAIL]: File /cre/versions.txt not found!"
+    exit 1
+fi
+
+if [ ! -f /cre/php-procfile ]; then
+    echo "[FAIL]: File /cre/php-procfile not found!"
+    exit 1
+fi
+
+if [ ! grep "crePhp" /cre/versions.txt > /dev/null]; then
+    echo "[FAIL]: php not installed!"
+    exit 1
+fi
+
+if [ ! grep -P "PHP $PHP_VERSION" /cre/versions.txt > /dev/null]; then
+    echo "[WARNING]: Wrong version of php installed!"
+    #exit 1
+fi
+
+#shoreman /cre/postgres-procfile &
+
+sleep 2
+
+#Do some fancy tests....?
+
+echo "[SUCCESS]"
+exit 0
