586ca9fe826a41a909cb99038288e6f6bc9b50e7
Unknown <michael.kahle@yahoo.de>
2019-02-20 11:25:01 +0100

    Adapt to new standard

---
+++ creMysql/cre/mysql-test.sh
@@ -0,0 +1,33 @@
+sleep 2
+
+if [ ! -f /cre/versions.txt ]; then
+    echo "[FAIL]: File /cre/versions.txt not found!"
+    exit 1
+fi
+
+if [ ! -f /cre/mysql-procfile ]; then
+    echo "[FAIL]: File /cre/mysql-procfile not found!"
+    exit 1
+fi
+
+if [ ! grep "postgres (PostgreSQL)" /cre/versions.txt > /dev/null]; then
+    echo "[FAIL]: postgres not installed!"
+    #exit 1
+fi
+
+if [ ! grep -P "postgres (PostgreSQL) POSTGRES_VERSION" /cre/versions.txt > /dev/null]; then
+    echo "[WARNING]: Wrong version of postgres installed!"
+    #exit 1
+fi
+
+if [ ! grep "psql (PostgreSQL)" /cre/versions.txt > /dev/null]; then
+    echo "[FAIL]: psql not installed!"
+    #exit 1
+fi
+
+sleep 2
+
+#Do some fancy tests....?
+
+echo "[SUCCESS]"
+exit 0
