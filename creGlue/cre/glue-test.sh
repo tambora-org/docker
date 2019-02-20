d55625a096bf38f5ac91ba850208a8bde9ac45a3
Unknown <michael.kahle@yahoo.de>
2019-02-20 09:39:58 +0100

    Rewrite test

--- creGlue/cre/glue-test.sh
+++ creGlue/cre/glue-test.sh
@@ -10,30 +10,14 @@ if [ ! -f /cre/glue-procfile ]; then
     exit 1
 fi
 
-if [! grep "docker-gen" /cre/versions.txt > /dev/null]; then
+if [ ! grep "docker-gen" /cre/versions.txt > /dev/null]; then
     echo "[FAIL]: docker-gen not installed!"
     exit 1
 fi
 
-if [! grep -P "docker-gen \T DOCKER_GEN_VERSION" /cre/versions.txt > /dev/null]; then
+if [ ! grep -P "docker-gen \T DOCKER_GEN_VERSION" /cre/versions.txt > /dev/null]; then
     echo "[WARNING]: Wrong version of docker-gen installed!"
     #exit 1
-fi
-
-shoreman /cre/glue-procfile &
-#docker-gen -watch /cre/glue/test.txt.tmpl /cre/glue/test.txt &
-
-sleep 2
-
-if [ ! -f /cre/glue/test.txt ]; then
-    echo "[FAIL]: File /cre/glue/test.txt not found!"
-    exit 1
-fi
-
-#actualsize=$(wc -c <"/cre/glue/test.txt")
-if [ ! $(wc -c <"/cre/glue/test.txt") -ge 200 ]; then
-    echo "[FAIL]: File /cre/glue/test.txt is too small!"
-    exit 1
 fi
 
 # Warn if the DOCKER_HOST socket does not exist
@@ -43,6 +27,23 @@ if [[ $DOCKER_HOST = unix://* ]]; then
         echo "[FAIL]: docker.sock not shared - see http://git.io/vZaG"
         exit 1
     fi
+fi
+
+shoreman /cre/glue-procfile &
+#docker-gen -watch /cre/glue/test.txt.tmpl /cre/glue/test.txt &
+/cre/gen-template.sh /cre/test.txt.tmpl &
+
+sleep 2
+
+if [ ! -f /cre/test.txt ]; then
+    echo "[FAIL]: File /cre/glue/test.txt not found!"
+    exit 1
+fi
+
+#actualsize=$(wc -c <"/cre/glue/test.txt")
+if [ ! $(wc -c <"/cre/test.txt") -ge 200 ]; then
+    echo "[FAIL]: File /cre/glue/test.txt is too small!"
+    exit 1
 fi
 
 echo "[SUCCESS]"
