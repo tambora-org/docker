1963013953dcabe564af5d8d1065a3408157e1eb
Unknown <michael.kahle@yahoo.de>
2019-02-26 15:03:44 +0100

    Use the right setting file

--- creProxy/cre/proxy-config.sh
+++ creProxy/cre/proxy-config.sh
@@ -3,7 +3,7 @@
 sleep 0.2
 if [ /cre/proxy.conf -nt /etc/nginx/conf.d/default.conf ]
 then
-   cp -f /cre/nginx-php.conf /etc/nginx/conf.d/default.conf
+   cp -f /cre/proxy.conf /etc/nginx/conf.d/default.conf
    sleep 0.1
    nginx -s reload
 fi
