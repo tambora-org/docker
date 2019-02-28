f89fe8530fc83efe34a858d708cd620e79fbbe4b
Unknown <michael.kahle@yahoo.de>
2019-02-28 09:16:52 +0100

    Move root dir from phyp to nginx-php

--- crePhp/cre/php-entrypoint.sh
+++ crePhp/cre/php-entrypoint.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 set -e
 
-# TODO: If CRE_PHP_ROOT not set: use www
-if [ -z $CRE_PHP_ROOT ]; then
-  CRE_PHP_ROOT='www';
-fi
-
-
-if [ ! -d "/cre/$CRE_PHP_ROOT" ]; 
-then
-  mkdir -p "/cre/$CRE_PHP_ROOT"
-fi
-
-cp -f /cre/db-test.php "/cre/$CRE_PHP_ROOT/db-test.php"
+echo "PHP entry"
 
 exec "$@"
