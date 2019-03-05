8737edb8248192e89787928debe6395d116cd013
Unknown <michael.kahle@yahoo.de>
2019-03-05 14:17:04 +0100

    Run composer in entrypoint

--- crePhpYii/cre/yii-entrypoint.sh
+++ crePhpYii/cre/yii-entrypoint.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -e 
 
+# /cre/php-entrypoint.sh # later maybe php-dev-entrypoint.sh
+
+composer create-project --prefer-dist yiisoft/yii2-app-basic /cre/tmp/yii 
+
 rsync -rl /cre/tmp/yii/ /cre/www/yii
 # later use --exclude /dir1/ --exclude /dir2/
 rm -rf /cre/tmp/yii
