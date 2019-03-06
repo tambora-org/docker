39383c028e6425bcdd12aa7ddb5336d4d4115fd0
Unknown <michael.kahle@yahoo.de>
2019-03-06 11:31:42 +0100

    Upgrade echo

--- crePostgres/cre/pg_hba.sh
+++ crePostgres/cre/pg_hba.sh
@@ -1,8 +1,9 @@
 #!/bin/sh 
 
-sleep 10
+sleep 12
 if [ /cre/pg_hba.conf -nt /etc/postgresql/${POSTGRES_VERSION}/main/pg_hba.conf ]
 then
+   echo "Activate Database Restrictions" 
    cp -f /cre/pg_hba.conf /etc/postgresql/${POSTGRES_VERSION}/main/pg_hba.conf
    sleep 0.15
    service postgresql restart
