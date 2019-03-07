8964632c5e3c993af04fe9da6a91de313d5d0173
Unknown <michael.kahle@yahoo.de>
2019-03-07 10:29:20 +0100

    Initialize vue development

---
+++ creDevVue/cre/vue-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e 
+
+# /cre/php-entrypoint.sh # later maybe php-dev-entrypoint.sh
+
+#composer create-project --prefer-dist yiisoft/yii2-app-basic /cre/tmp/yii 
+mkdir cre/tmp/vue
+cd cre/tmp/vue
+npm init
+npm install --save-dev vue vue-router
+
+
+##rsync -r /cre/tmp/yii/ /cre/www/yii
+# later use --exclude /dir1/ --exclude /dir2/
+##rm -rf /cre/tmp/yii
+
+##chmod -R 774 /cre/www/yii/web/assets 
+##chown -R www-data:www-data /cre/www/yii/web/assets
+
+exec "$@"
