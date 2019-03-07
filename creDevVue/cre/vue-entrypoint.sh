0e6003367e84d7e9be6240c4afb14141afe8329e
Unknown <michael.kahle@yahoo.de>
2019-03-07 12:12:25 +0100

    Add webpack

--- creDevVue/cre/vue-entrypoint.sh
+++ creDevVue/cre/vue-entrypoint.sh
@@ -8,11 +8,15 @@ cd /cre/tmp/cre-vue
 
 ## npm set init.author.email "example-user@example.com"
 ## npm set init.author.name "example_user"
-## npm set init.license "MIT"
+## npm config set init.author.url http://iamsim.me/
+## npm set init-version "${CRE_VERSION}.0"
+npm set init.license "Apache-2.0"
 
 # https://docs.npmjs.com/creating-a-package-json-file#default-values-extracted-from-the-current-directory
 npm init -y
 npm install --save-dev vue vue-router
+npm install --save-dev webpack webpack-cli
+
 
 ##rsync -r /cre/tmp/yii/ /cre/www/yii
 # later use --exclude /dir1/ --exclude /dir2/
