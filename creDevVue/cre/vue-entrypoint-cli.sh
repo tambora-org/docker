6335c6ec728e07d9e7b86816333f8741c3594db0
Unknown <michael.kahle@yahoo.de>
2019-03-08 09:27:35 +0100

    Use cli 3.0

--- creDevVue/cre/vue-entrypoint-cli.sh
+++ creDevVue/cre/vue-entrypoint-cli.sh
@@ -4,7 +4,7 @@ set -e
 # /cre/php-entrypoint.sh # later maybe php-dev-entrypoint.sh
 
 mkdir -p /cre/dev/cre-vue
-cd /cre/dev/cre-vue
+cd /cre/dev
 
 ## npm set init.author.email "example-user@example.com"
 ## npm set init.author.name "example_user"
@@ -13,25 +13,27 @@ npm set init.version "${CRE_VERSION}.0"
 npm set init.license "Apache-2.0"
 
 # https://docs.npmjs.com/creating-a-package-json-file#default-values-extracted-from-the-current-directory
-npm init -y
+#npm init -y
 npm install -g npm-add-script # -g needed; https://www.npmjs.com/package/npm-add-script 
-npm install -g vue-cli 
-npm install --save-dev vue vue-router       #vue-vuex
-npm install --save-dev webpack webpack-cli
-npm install --save-dev vue-loader vue-template-compiler 
-npm install --save-dev vue-style-loader css-loader stylus stylus-loader
-npm install --save-dev webpack-dev-server html-webpack-plugin copy-webpack-plugin
-npm install --save-dev @babel/core babel-loader @babel/preset-env  #js-ES5
-# install static code-check
-npm install --save-dev eslint babel-eslint eslint-loader eslint-config-standard 
-npm install --save-dev eslint-plugin-standard eslint-plugin-import eslint-plugin-node eslint-plugin-promise eslint-plugin-vue
+npm install -g @vue/cli       # @ for 3.0 
 
+vue create -d -f cre-vue
+
+cd /cre/dev/cre-vue
+
+##npm install --save-dev vue vue-router       #vue-vuex
+##npm install --save-dev webpack webpack-cli
+##npm install --save-dev vue-loader vue-template-compiler 
+##npm install --save-dev vue-style-loader css-loader stylus stylus-loader
+##npm install --save-dev webpack-dev-server html-webpack-plugin copy-webpack-plugin
+##npm install --save-dev @babel/core babel-loader @babel/preset-env  #js-ES5
+# install static code-check
+##npm install --save-dev eslint babel-eslint eslint-loader eslint-config-standard 
+##npm install --save-dev eslint-plugin-standard eslint-plugin-import eslint-plugin-node eslint-plugin-promise eslint-plugin-vue
 
 ##npmAddScript -k "dev" -v "webpack-dev-server --config build/webpack.config.dev.js"
 ##npmAddScript -k "cre" -v "webpack --config build/webpack.config.dev.js"
 ##npmAddScript -k "eslint" -v "eslint --ext .js,.vue, src"
-
-vue create -d cre-app
 
 #npm run dev ## run in procfile
 
