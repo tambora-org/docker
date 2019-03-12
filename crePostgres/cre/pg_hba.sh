caae1748940073f9c6ae365e696e14d956be135f
Unknown <michael.kahle@yahoo.de>
2019-03-12 13:27:48 +0100

    Busy file to avoid blocking

--- crePostgres/cre/pg_hba.sh
+++ crePostgres/cre/pg_hba.sh
@@ -1,6 +1,13 @@
 #!/bin/sh 
 
 sleep 12
+
+until [ ! -e /cre/database-busy.txt ]
+do
+ echo "Database is busy"
+ sleep 2;
+done
+
 if [ /cre/pg_hba.conf -nt /etc/postgresql/${POSTGRES_VERSION}/main/pg_hba.conf ]
 then
    echo "Activate Database Restrictions" 
