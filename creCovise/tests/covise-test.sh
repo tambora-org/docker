7c842e1d0214252f443d66d2de656f9f467a268b
Unknown <michael.kahle@yahoo.de>
2019-05-14 12:22:18 +0200

    tagging and test

---
+++ creCovise/tests/covise-test.sh
@@ -0,0 +1,30 @@
+sleep 2
+
+if [ ! -f /cre/versions.txt ]; then
+    echo "[FAIL]: File /cre/versions.txt not found!"
+    #exit 1
+fi
+
+if [ ! -f /cre/nginx-procfile ]; then
+    echo "[FAIL]: File /cre/nginx-procfile not found!"
+    #exit 1
+fi
+
+isInFile=$(cat /cre/versions.txt | grep -c "creNginx")
+if [ $isInFile -eq 0 ]; then
+    echo "[FAIL]: nginx not installed!"
+    #exit 1
+fi
+
+isInFile=$(cat /cre/versions.txt | grep -cP "creNginx \t $NGINX_VERSION")
+if [ $isInFile -eq 0 ]; then
+    echo "[WARNING]: Wrong version of nginx installed!"
+    #exit 1
+fi
+
+sleep 2
+
+#Do some fancy tests....?
+
+echo "[SUCCESS]"
+exit 0
---
+++ crePython/tests/python-test.sh
@@ -0,0 +1,30 @@
+sleep 2
+
+if [ ! -f /cre/versions.txt ]; then
+    echo "[FAIL]: File /cre/versions.txt not found!"
+    exit 1
+fi
+
+if [ ! -f /cre/python-procfile ]; then
+    echo "[FAIL]: File /cre/python-procfile not found!"
+    #exit 1
+fi
+
+isInFile=$(cat /cre/versions.txt | grep -c "crePython")
+if [ $isInFile -eq 0 ]; then
+    echo "[FAIL]: python not installed!"
+    #exit 1
+fi
+
+isInFile=$(cat /cre/versions.txt | grep -cP "crePython \t $PYTHON_VERSION")
+if [ $isInFile -eq 0 ]; then
+    echo "[WARNING]: Wrong version of python installed!"
+    #exit 1
+fi
+
+sleep 2
+
+#Do some fancy tests....?
+
+echo "[SUCCESS]"
+exit 0
