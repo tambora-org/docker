8d898739adfba6824007f9a43498b0f418985038
Michael Kahle <michael.kahle@yahoo.de>
2019-06-18 09:37:26 +0200

    add secret mechanism

---
+++ creGlue/cre/get-secret.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Returns first line of secret file (used for passwords, etc)
+# (First parameter is path&file or file in /cre/secrets
+
+secret_file=$1
+secret_path=/cre/secrets
+
+if [ -f $secret_file ]; then
+    secret_path_and_file=$secret_file
+else
+    secret_path_and_file=$secret_path/$secret_file
+fi
+
+if [ ! -f $secret_path_and_file ]; then
+    echo "[FAIL]: Secret file $secret_path_and_file not found !"
+    exit 1
+fi
+
+first_line="$(head -n 1 $secret_path_and_file)"
+echo "$first_line"
