c740ef54eca53c7c86ec467b6656b5685921dca1
Michael Kahle <michael.kahle@yahoo.de>
2019-06-18 09:56:56 +0200

    Use secret concept for mail passwords

--- creGlue/cre/get-secret.sh
+++ creGlue/cre/get-secret.sh
@@ -2,6 +2,7 @@
 
 # Returns first line of secret file (used for passwords, etc)
 # (First parameter is path&file or file in /cre/secrets
+# (Second optional parameter is fallback value)
 
 secret_file=$1
 secret_path=/cre/secrets
@@ -13,8 +14,12 @@ else
 fi
 
 if [ ! -f $secret_path_and_file ]; then
-    echo "[FAIL]: Secret file $secret_path_and_file not found !"
-    exit 1
+    if [ -z "$2" ]
+      echo "[FAIL]: Secret file $secret_path_and_file not found !"
+      exit 1
+    else
+      echo "$2"
+      exit 0
 fi
 
 first_line="$(head -n 1 $secret_path_and_file)"
