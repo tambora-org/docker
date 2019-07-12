33cf51957675af7ba75f9d7a88a0631ec423594e
Michael Kahle <michael.kahle@yahoo.de>
2019-07-12 17:21:13 +0200

    Remove semicolon

--- crePhp/cre/php-fpm.sh
+++ crePhp/cre/php-fpm.sh
@@ -2,8 +2,8 @@
 
 if [ $(echo " $PHP_VERSION < 7.2" | bc) -eq 1 ] ; then
   /usr/sbin/php-fpm7.0 -F -O 2>&1 | sed -u 's,.*: \"\(.*\)$,\1,'| sed -u 's,"$,,' 1>&1
-; else
+else
   /usr/sbin/php-fpm7.2 -F -O 2>&1 | sed -u 's,.*: \"\(.*\)$,\1,'| sed -u 's,"$,,' 1>&1
-; fi
+fi
 
 #or use if file exists -e 
