5cfd780cf94ef6cf93e672342ec59fbdb668fb26
Michael Kahle <michael.kahle@yahoo.de>
2020-01-24 09:19:55 +0100

    Adapt test, build & push tag

--- creMysql/tests/mysql-test.sh
+++ creMysql/tests/mysql-test.sh
@@ -5,6 +5,8 @@ if [ ! -f /cre/versions.txt ]; then
     exit 1
 fi
 
+cat /cre/versions.txt
+
 if [ ! -f /cre/mysql-procfile ]; then
     echo "[FAIL]: File /cre/mysql-procfile not found!"
     exit 1
