4e1976aa9b57d57ff32a1f5e9907ba276f700b23
Unknown <michael.kahle@yahoo.de>
2020-02-22 12:28:54 +0100

    Add module for Rust Wasm

---
+++ crePhpRust/tests/php-rust-test.sh
@@ -0,0 +1,33 @@
+sleep 2
+
+if [ ! -f /cre/versions.txt ]; then
+    echo "[FAIL]: File /cre/versions.txt not found!"
+    exit 1
+fi
+
+cat /cre/versions.txt
+
+if [ ! -f /cre/php-procfile ]; then
+    echo "[FAIL]: File /cre/php-procfile not found!"
+    exit 1
+fi
+
+isInFile=$(cat /cre/versions.txt | grep -c "crePhp")
+if [ $isInFile -eq 0 ]; then
+    echo "[FAIL]: Php not installed!"
+    exit 1
+fi
+
+isInFile=$(cat /cre/versions.txt | grep -cP "PHP $PHP_VERSION")
+if [ $isInFile -eq 0 ]; then
+    echo "[WARNING]: Wrong version of php installed!"
+    #exit 1
+fi
+
+
+sleep 2
+
+#Do some fancy tests....?
+
+echo "[SUCCESS]"
+exit 0
