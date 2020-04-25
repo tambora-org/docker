029ab28dce7f41cf2fd29a8bea358f6e9413065e
Unknown <michael.kahle@yahoo.de>
2020-04-25 19:14:19 +0200

    directory adapted

--- crePhpYii3b/cre/yii3-entrypoint.sh
+++ crePhpYii3b/cre/yii3-entrypoint.sh
@@ -6,12 +6,14 @@ set -e
 # https://www.yiiframework.com/wiki/2547/draft-understanding-yii-3
 
 ##rsync -rl /cre/tmp/yii/ /cre/www/yii
-rsync -r /cre/tmp/yii/ /cre/www/yii
+rsync -r /cre/tmp/yii-demo/ /cre/www/yii
 # later use --exclude /dir1/ --exclude /dir2/
 ##rm -rf /cre/tmp/yii
 
 ##chmod -R 774 /cre/www/yii/public/assets 
 ##chown -R www-data:www-data /cre/www/yii/public/assets
+
+cd /cre/www/yii-demo && composer install && composer dump-autoload
 
 exec "$@"
 
