f66676d0d708a49ce0cceb5fdcea7d4cdfef440c
Unknown <michael.kahle@yahoo.de>
2020-04-25 12:41:50 +0200

    Split yii-demo and yii-template

---
+++ crePhpYii3c/cre/yii3-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e 
+
+# call ? /cre/php-entrypoint.sh # later maybe php-dev-entrypoint.sh
+
+# https://www.yiiframework.com/wiki/2547/draft-understanding-yii-3
+
+##rsync -rl /cre/tmp/yii/ /cre/www/yii
+rsync -r /cre/tmp/yii/ /cre/www/yii
+# later use --exclude /dir1/ --exclude /dir2/
+##rm -rf /cre/tmp/yii
+
+##chmod -R 774 /cre/www/yii/public/assets 
+##chown -R www-data:www-data /cre/www/yii/public/assets
+
+cd /cre/www/yii && composer install && composer dump-autoload
+
+exec "$@"
+
