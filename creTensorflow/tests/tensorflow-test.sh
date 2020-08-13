991bcfe67758c96bdfbafee8b57aa038174f04e5
Unknown <michael.kahle@yahoo.de>
2020-08-14 00:03:08 +0200

    display versions.txt in test

--- creTensorflow/tests/tensorflow-test.sh
+++ creTensorflow/tests/tensorflow-test.sh
@@ -5,6 +5,8 @@ if [ ! -f /cre/versions.txt ]; then
     exit 1
 fi
 
+cat /cre/versions.txt
+
 if [ ! -f /cre/python-procfile ]; then
     echo "[FAIL]: File /cre/python-procfile not found!"
     #exit 1
