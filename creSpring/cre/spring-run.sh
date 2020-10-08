e30549cad3330ecf2cf62c1383d05f541d6f4b99
Unknown <michael.kahle@yahoo.de>
2020-10-08 20:08:01 +0200

    First install then copy to initialize

--- creSpring/cre/spring-run.sh
+++ creSpring/cre/spring-run.sh
@@ -1,5 +1,10 @@
 #!/bin/sh 
 
+cp -R -f /cre/maven-spring-boot /cre/spring/app
+##RUN mv /cre/maven-spring-boot /cre/spring/app
+cp -f /cre/DemoApplication.java /cre/spring/app/src/main/java/com/cre/demo/DemoApplication.java
+
+
 sleep 2.25 
 /cre/spring/app/mvnw spring-boot:run 
 
