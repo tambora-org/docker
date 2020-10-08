92ad7fc15a7179880edb9a13e303d3d903493887
Unknown <michael.kahle@yahoo.de>
2020-10-08 21:02:59 +0200

    overwrite target directory

--- creSpring/cre/application.properties.sh
+++ creSpring/cre/application.properties.sh
@@ -3,7 +3,7 @@
 sleep 0.25
 if [ /cre/application.properties -nt /cre/spring/app/src/main/resources/application.properties ]
 then
-   echo "Now copy latest application.properties to /cre/www/"
+   echo "Now copy latest application.properties to /cre/spring/app/src/main/resources"
    cp -f /cre/application.properties /cre/spring/app/src/main/resources/application.properties
 fi
 
--- creSpring/cre/spring-run.sh
+++ creSpring/cre/spring-run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh 
 
-cp -R -f /cre/maven-spring-boot /cre/spring/app
-##RUN mv /cre/maven-spring-boot /cre/spring/app
+echo "Initialize spring-boot to /cre/spring/app"
+cp -R -T -f /cre/maven-spring-boot /cre/spring/app
 cp -f /cre/DemoApplication.java /cre/spring/app/src/main/java/com/cre/demo/DemoApplication.java
 
-
 sleep 2.25 
+echo "Now run spring"
 /cre/spring/app/mvnw spring-boot:run 
 
 
