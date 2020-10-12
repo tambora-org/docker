921ed99103c2d9637f21fc2c8d52ba6f9fe4b070
KMicha <michael.kahle@yahoo.de>
2020-10-12 13:23:18 +0200

    sleep for db

--- creSpring/cre/spring-run.sh
+++ creSpring/cre/spring-run.sh
@@ -5,6 +5,11 @@ cp -R -T -f /cre/maven-spring-boot /cre/spring/app
 cp -f /cre/DemoApplication.java /cre/spring/app/src/main/java/com/cre/demo/DemoApplication.java
 
 sleep 2.25 
+
+# wait for db.... / just sleep for now
+sleep 15
+
+
 echo "Now run spring"
 /cre/spring/app/mvnw spring-boot:run 
 
