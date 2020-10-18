e7ed9f37c06bcca359d9030dd1195338bcb3610c
Unknown <michael.kahle@yahoo.de>
2020-10-18 13:58:18 +0200

    split properties for postgresql and mail

--- creSpring/cre/application.properties.sh
+++ creSpring/cre/application.properties.sh
@@ -1,6 +1,19 @@
 #!/bin/sh 
 
 sleep 0.25
+
+if [ /cre/application-pgs.properties -nt /cre/spring/app/src/main/resources/application-pgs.properties ]
+then
+   echo "Now copy latest application-pgs.properties to /cre/spring/app/src/main/resources"
+   cp -f /cre/application-pgs.properties /cre/spring/app/src/main/resources/application-pgs.properties
+fi
+
+if [ /cre/application-mail.properties -nt /cre/spring/app/src/main/resources/application-mail.properties ]
+then
+   echo "Now copy latest application-mail.properties to /cre/spring/app/src/main/resources"
+   cp -f /cre/application-mail.properties /cre/spring/app/src/main/resources/application-mail.properties
+fi
+
 if [ /cre/application.properties -nt /cre/spring/app/src/main/resources/application.properties ]
 then
    echo "Now copy latest application.properties to /cre/spring/app/src/main/resources"
