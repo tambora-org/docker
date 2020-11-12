9498dfac02770542ae777879e2f623e850cebcf8
Unknown <michael.kahle@yahoo.de>
2020-11-12 11:19:45 +0100

    ports etc....

--- creRabbitmq/cre/start-rabbitmq.sh
+++ creRabbitmq/cre/start-rabbitmq.sh
@@ -1,6 +1,12 @@
 #!/bin/sh 
 
-/usr/bin/java -jar /usr/share/jenkins/jenkins.war --argumentsRealm.passwd.jenkins=swordfish --argumentsRealm.roles.jenkins=admin &
+# https://www.rabbitmq.com/configure.html
+# https://www.rabbitmq.com/configure.html#configuration-encryption
+
+
+
+# /usr/bin/java -jar /usr/share/jenkins/jenkins.war --
+# argumentsRealm.passwd.jenkins=swordfish --argumentsRealm.roles.jenkins=admin &
 #/usr/bin/java -jar /var/jenkins/war/WEB-INF/jenkins-cli.jar -s http://127.0.0.1:8080 install-plugin ant --username jenkins --password swordfish
 
 #run the CMD part now
