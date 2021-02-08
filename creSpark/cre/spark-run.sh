52c45108e9aa9c3c9a6617cc26c0f095ca5ae182
Unknown <michael.kahle@yahoo.de>
2021-02-08 17:57:16 +0100

    add spark

---
+++ creSpark/cre/spark-run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh 
+
+echo "Initialize spring-boot to /cre/spring/app"
+cp -R -T -f /cre/maven-spring-boot /cre/spring/app
+cp -f /cre/DemoApplication.java /cre/spring/app/src/main/java/com/cre/demo/DemoApplication.java
+
+sleep 2.25 
+
+# wait for db.... / just sleep for now
+sleep 15
+
+
+echo "Now run spring"
+/cre/spring/app/mvnw spring-boot:run 
+
+
