daeb7adfd412ef32d673d7a2f7c187a693f59b7f
Unknown <michael.kahle@yahoo.de>
2021-04-11 19:20:54 +0200

    add test

---
+++ creImage/tests/image-test.sh
@@ -0,0 +1,15 @@
+sleep 2
+
+if [ ! -f /cre/versions.txt ]; then
+    echo "[FAIL]: File /cre/versions.txt not found!"
+    exit 1
+fi
+
+cat /cre/versions.txt
+
+sleep 2
+
+#Do some fancy tests....?
+
+echo "[SUCCESS]"
+exit 0
