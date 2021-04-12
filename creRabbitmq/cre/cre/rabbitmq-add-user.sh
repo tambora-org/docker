ba988623814f200b01383830d126af5e3c79aa16
Unknown <michael.kahle@yahoo.de>
2021-04-12 17:41:48 +0200

    first preparation for message setup

---
+++ creRabbitmq/cre/cre/rabbitmq-add-user.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#rabbitmqctl add_user my_user my_password
+#rabbitmqctl set_permissions my_user ".*" ".*" ".*"
+##rabbitmqctl set_permissions -p / my_user ".*" ".*" ".*"
+#rabbitmqctl set_user_tags my_user administrator
+#rabbitmqctl set_user_tags my_user management
