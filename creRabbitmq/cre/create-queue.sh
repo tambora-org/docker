281059f5a73c0c98c05b442763458f1941b46259
Unknown <michael.kahle@yahoo.de>
2021-04-16 17:53:29 +0200

    brackets wrong

--- creRabbitmq/cre/create-queue.sh
+++ creRabbitmq/cre/create-queue.sh
@@ -5,9 +5,9 @@ echo "Message: $line"
 
 #initQueue='{"exchange":"cre.default", "binding":"cre.image.demo", "queue":"cre.image.show"}'
 
-exchange=${echo $line | jq ".exchange"}
-binding=${echo $line | jq ".binding"}
-queue=${echo $line | jq ".queue"}
+exchange=$(echo $line | jq '.exchange')
+binding=$(echo $line | jq '.binding')
+queue=$(echo $line | jq '.queue')
 
 rabbitmqadmin declare exchange name="$exchange" type=direct  durable=false auto_delete=true
 rabbitmqadmin declare queue name="$queue" durable=false auto_delete=true
