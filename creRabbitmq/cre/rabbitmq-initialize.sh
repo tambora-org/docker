33c81f29724b05c4e4c1d44dd83863fc7938d5b5
Unknown <michael.kahle@yahoo.de>
2021-04-16 11:45:55 +0200

    auto_delete

--- creRabbitmq/cre/rabbitmq-initialize.sh
+++ creRabbitmq/cre/rabbitmq-initialize.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
- 
+
+sleep 3 
 echo "initialize rabbitmq server"
-
 rabbitmqctl await_startup
-
+sleep 1
 echo "rabbitmq server running"
 
 #munchausen-swamp
-rabbitmqadmin declare exchange name="cre.swamp" type=direct
-rabbitmqadmin declare queue name="cre.munchausen" durable=false
+rabbitmqadmin declare exchange name="cre.swamp" type=direct  durable=false auto_delete=true
+rabbitmqadmin declare queue name="cre.munchausen" durable=false auto_delete=true
 rabbitmqadmin declare binding source="cre.swamp" destination="cre.munchausen" routing_key="cre.horse" 
 ## [destination_type=... arguments=...]
 
-amqp-consume -q "cre.swamp" -e "cre.munchausen" --vhost "/" -r "cre.horse" -d ./show-message.sh
+amqp-consume -e "cre.swamp" -q "cre.munchausen" -r "cre.horse" --vhost "/" -d ./show-message.sh
 
