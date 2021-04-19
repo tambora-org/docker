ded3e0b6f7e6886f224d0c8fc1e7b8d20f109ee5
Unknown <michael.kahle@yahoo.de>
2021-04-19 12:04:10 +0200

    queue

--- creImage/cre/init-consume.sh
+++ creImage/cre/init-consume.sh
@@ -5,10 +5,10 @@
 #initQueue='{"exchange":"cre.default", "binding":"cre.image.demo", "queue":"cre.image.show"}'
  
 #amqp-publish --url=amqp://admin:secret@172.18.0.4:5672 -e "cre.swamp" -r "cre.horse" -b "$initQueue"
-/cre/create-queue.sh -e "cre.default" -q "cre.image.show" -r "cre.image.demo"
+/cre/create-queue.sh -e "cre.default" -q "cre.image.show" -b "cre.image.demo"
 
 sleep 1
 
 #amqp-consume --url=amqp://admin:secret@172.18.0.4:5672 -e "cre.default" -q "cre.image.show" -r "cre.image.demo" -d /cre/show-message.sh
-/cre/consume-queue.sh -e "cre.default" -q "cre.image.show" -r "cre.image.demo" -d /cre/show-message.sh
+/cre/consume-queue.sh -e "cre.default" -q "cre.image.show" -b "cre.image.demo" -d /cre/show-message.sh
 
