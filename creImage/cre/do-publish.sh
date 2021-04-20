a48721fa21a532e3a4e2f44810fe89b828d8e3ad
Unknown <michael.kahle@yahoo.de>
2021-04-20 16:45:53 +0200

    minor bugs

--- creImage/cre/do-publish.sh
+++ creImage/cre/do-publish.sh
@@ -4,4 +4,4 @@
 
 #amqp-publish --url=amqp://admin:secret@172.18.0.4:5672 -e "cre.default" -r "cre.image.demo" -b "this is a test message"
 
-/cre/publish-queueu.sh -e "cre.default" -b "cre.image.demo" -m "this is a test message"
+/cre/publish-queue.sh -e "cre.default" -b "cre.image.demo" -m "this is a test message"
