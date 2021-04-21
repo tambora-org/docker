e72897e70b78c8611d615dd2d499cd2c4c9f58ce
Unknown <michael.kahle@yahoo.de>
2021-04-21 22:16:58 +0200

    need to wait longer for rabbit to start

--- creImage/cre/init-consume.sh
+++ creImage/cre/init-consume.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #https://wlanboy.com/tutorial/rabbitmq-bash/
 
+#somehow check remote status?
+sleep 10
 sleep 5
 /cre/mq/create-queue.sh -e "cre.default" -q "cre.image.show" -b "cre.image.demo"
 
