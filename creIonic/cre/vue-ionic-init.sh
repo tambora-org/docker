9e7ebd3ee5d4c69dc3e806a2657b2dd3edf46302
Unknown <michael.kahle@yahoo.de>
2021-04-27 12:18:14 +0200

    ionic cmd

--- creIonic/cre/vue-ionic-init.sh
+++ creIonic/cre/vue-ionic-init.sh
@@ -3,8 +3,9 @@
 lassign $argv appDir appName
 
 #git clone https://github.com/KMicha/cre-test.git $appDir
-##ionic start $appDir tabs
-/cre/node/.npm-global/bin/ionic start $appDir tabs
+ionic start $appDir tabs
+##/cre/node/.npm-global/bin/ionic start $appDir tabs
+# /cre/node/.npm-global/bin/ionic -> /cre/node/.npm-global/lib/node_modules/ionic/bin/ionic
 cd cre-test && npm install 
 
 ## && ionic serve
