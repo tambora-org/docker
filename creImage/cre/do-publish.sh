60e772d65fb320bcba8a4bb93c745916d6b41641
KMicha <michael.kahle@yahoo.de>
2022-06-03 08:54:54 +0200

    rm creImage (moved to creDocker)

--- creImage/cre/do-publish.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#https://wlanboy.com/tutorial/rabbitmq-bash/
-
-#amqp-publish --url=amqp://admin:secret@172.18.0.4:5672 -e "cre.default" -r "cre.image.demo" -b "this is a test message"
-
-/cre/mq/publish-queue.sh -e "cre.default" -b "cre.image.demo" -m "this is a test message"
